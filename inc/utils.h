278ddcbff9bcc9297fa85e607610b72d56c33688
Michele Carenza <michele.carenza@studenti.unipr.it>
2023-09-22 23:41:07 +0200

    remoce commented code

--- inc/utils.h
+++ inc/utils.h
@@ -11,8 +11,5 @@ enum Error {
 
 void spin(volatile uint32_t count);
 char* int_to_string(int n);
-// void set_register_bits(volatile uint32_t *periph, uint32_t reg, uint32_t bits);
-// uint32_t get_register_bits(volatile uint32_t *periph, uint32_t reg, uint32_t bits);
-// int register_check(volatile uint32_t *periph, uint32_t reg);
 
 #endif
